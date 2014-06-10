a788da2fec3f9bfe7e631b296de972b0554ee60e
zhbanton <zhbanton@gmail.com>
2014-06-10 12:09:14 -0400

    Complete lab 1

---
+++ lab1.rb
@@ -0,0 +1,10 @@
+days = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+days.unshift(days.pop)
+days[days.index('Thursday')].upcase!
+days2 = [days[1..5], [days[0], days[-1]]]
+days2[0][index('Wednesday')] = "Woden's Day"
+days2.shift
+days.sort!
+days.each do |day|
+  puts day
+end
